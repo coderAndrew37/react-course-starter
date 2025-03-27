81ab262872808f98966759398a2013be25979dd4
coderAndrew <omolloandrew37@gmail.com>
2025-03-27 07:41:00 +0300

    using raect dev tools

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -17,6 +17,7 @@ const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: ["todos"], // This is the key for the query ie unique identifier
     queryFn: fetchTodos, // This is the function that will be called to fetch the data
+    staleTime: 10 * 1000, // This is the time in milliseconds after which the data will be considered stale
   });
 };
 
