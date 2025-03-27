95d4331580ca9ce7be5fc85d1c300f5de4c4ed5c
coderAndrew <omolloandrew37@gmail.com>
2025-03-27 07:08:59 +0300

    Using react query to fetch data

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoList from "./react-query/TodoList";
+
+function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const TodoList = () => {
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+
+  //Implementing catching using react-query
+  const { data: todos, error } = useQuery<Todo[], Error>({
+    queryKey: ["todos"], // This is the key for the query ie unique identifier
+    queryFn: fetchTodos, // This is the function that will be called to fetch the data
+  });
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todos?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
