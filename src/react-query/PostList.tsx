7da64f3803112d2479a86735f714915cf4459343
coderAndrew <omolloandrew37@gmail.com>
2025-03-27 10:02:47 +0300

    Filtering by user

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import usePosts from "../hooks/usePosts";
 
 const PostList = () => {
-  const { data: posts, error, isLoading } = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
 
   if (error) return <p>{error.message}</p>;
   if (isLoading)
@@ -11,14 +13,31 @@ const PostList = () => {
       </div>
     );
 
+  const handleUserOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setUserId(parseInt(e.target.value));
+  };
+
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="form-select mb-3"
+        onChange={handleUserOnChange}
+        value={userId}
+      >
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
